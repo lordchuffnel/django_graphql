7ee75c285328ba4bd1841e8276cb123bf87006f5
Frank Huff <fs-j@live.com>
2020-01-20 11:13:32 -0800

    added mutations

--- api/schema.py
+++ api/schema.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from api.models import Movie
+from api.models import Movie, Director
 
 
 class MovieType(DjangoObjectType):
@@ -13,12 +13,22 @@ class MovieType(DjangoObjectType):
   def resolve_movie_age(self, info):
     return "old movie" if self.year < 2000 else "new movie"
 
+
+class DirectorType(DjangoObjectType):
+  class Meta:
+    model = Director
+
 class Query(graphene.ObjectType):
   all_movies = graphene.List(MovieType)
-  movie = graphene.Field(MovieType, id=graphene.Int(),j title=graphene.String())
+  movie = graphene.Field(MovieType, id=graphene.Int(), jtitle=graphene.String())
+  
+  all_directors = graphene.List(DirectorType)
 
   def resolve_all_movies(self, info, **kwargs):
     return Movie.objects.all()
+
+  def resolve_all_directors(self, info, **kwargs):
+    return Director.objects.all()
 
   def resolve_movie(self, info, **kwargs):
     id = kwargs.get('id')
@@ -31,3 +41,18 @@ class Query(graphene.ObjectType):
       return Movie.objects.get(title=title)
 
     return None
+
+class MovieCreateMutation(graphene.Mutation):
+  class Arguments:
+    title = graphene.String(required=True)
+    year = graphene.Int(required=True)
+
+  movie = graphene.Field(MovieType)
+
+  def mutate(self, info, title, year):
+    movie = Movie.objects.create(title=title, year=year)
+
+    return MovieCreateMutation(movie=movie)
+
+class Mutation:
+  create_move = MovieCreateMutation.Field()
--- movies/schema.py
+++ movies/schema.py
@@ -1,7 +1,13 @@
 import graphene
 import api.schema
 
+
+
 class Query(api.schema.Query, graphene.ObjectType):
   pass
 
-schema = graphene.Schema(query=Query)
+class Mutation(api.schema.Mutation, graphene.ObjectType):
+  pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
