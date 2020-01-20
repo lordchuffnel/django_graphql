5cc935c533cfea6d5e42e26add94337658211524
Frank Huff <fs-j@live.com>
2020-01-20 10:10:27 -0800

    second commit

--- api/models.py
+++ api/models.py
@@ -3,5 +3,8 @@ from django.db import models
 
 
 class Movie(models.Model):
-  title = models.Charfield(max_length=32)
+  title = models.CharField(max_length=32)
   year = models.IntegerField(default=2000)
+
+  def __str__(self):
+    return self.title
